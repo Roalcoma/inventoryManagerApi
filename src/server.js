d32a2bc64c36e25f98ae9c817645a63e6005a192
Rodrigo Cova <78998470+Roalcoma@users.noreply.github.com>
2025-02-01 19:07:00 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import { app } from './app.js';
 
-const PORT = process.env.PORT || Math.floor(Math.random() * 1000) + 3000;
+const PORT = process.env.PORT || Math.floor(Math.random() * 1000) + 9000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
